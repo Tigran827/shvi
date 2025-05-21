0cf5cd9484891d6fa7313da256d3e209293ce308
Tigran <nagdalyantigran03@gmail.com>
2025-05-21 17:58:04 +0400

    tokenizing a string

--- sintez.js
+++ sintez.js
@@ -66,6 +66,13 @@ async function encodeWAV(
 
 const atom = (name) => Symbol.for(name);
 
-const tokenize = (input) => {
-  throw new Error("Not implemented");
-};
+function tokenize(input) {
+  if (!input.trim()) return [];
+
+  const words = input.trim().split(/\s+/);
+
+  return words.map((word) => {
+    const num = Number(word);
+    return isNaN(num) ? atom(word) : num;
+  });
+}
